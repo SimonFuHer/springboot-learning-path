e0cca0461a3ce5f432a69caf430a82ed2b02b699
SimonFuHer <simonfuentes2005@gmail.com>
2025-12-24 17:22:15 +0100

    feat: Primer endpoint hola mundo funcional

--- src/main/java/com/simon/primer_api/PrimerApiApplication.java
+++ src/main/java/com/simon/primer_api/PrimerApiApplication.java
@@ -2,12 +2,20 @@ package com.simon.primer_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController // <--- 1. Anotación nueva: "Esta clase ahora escucha peticiones web"
 public class PrimerApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrimerApiApplication.class, args);
 	}
 
+	// 2. Método nuevo: Cuando alguien entre a la raíz, responde esto
+	@GetMapping("/saludo")
+	public String holaMundo() {
+		return "¡Hola Simón! Tu primera API con Spring Boot está viva 🚀";
+	}
 }
