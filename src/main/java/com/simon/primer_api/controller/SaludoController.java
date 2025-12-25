d9cedac27ff6a163912573120004d38c30d0841f
SimonFuHer <simonfuentes2005@gmail.com>
2025-12-25 14:07:59 +0100

    feat: Añadir soporte para nombres dinamicos en la URL y metodo por defecto

--- src/main/java/com/simon/primer_api/controller/SaludoController.java
+++ src/main/java/com/simon/primer_api/controller/SaludoController.java
@@ -1,6 +1,6 @@
 package com.simon.primer_api.controller;
 
-import com.simon.primer_api.model.Saludo; // <--- Importante: Importar tu nueva clase
+import com.simon.primer_api.model.Saludo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,15 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SaludoController {
 
-    // Fíjate en el cambio: añadimos /{nombre} entre llaves
+    // 1. ESCENARIO A: Si ponen nombre
     @GetMapping("/saludo/{nombre}")
     public Saludo holaMundo(@PathVariable String nombre) {
-        // @PathVariable conecta la URL con la variable 'nombre' de Java
+        return new Saludo("Hola " + nombre, "Simón", "¡Enhorabuena " + nombre + ", tu API ya responde dinámicamente!");
+    }
 
-        return new Saludo(
-                "Hola " + nombre,
-                "Simón",
-                "¡Enhorabuena " + nombre + ", tu API ya responde dinámicamente!"
-        );
+    // 2. ESCENARIO B: Si NO ponen nombre (El "Plan B")
+    @GetMapping("/saludo")
+    public Saludo saludoDefault() {
+        return new Saludo("Hola Desconocido", "Simón", "No me has dicho tu nombre, ¡pero te saludo igual!");
     }
 }
